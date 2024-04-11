cf769f76f71a5bddba9c3a1de2f11b678aad38ef
James Chen <jchen.6727@gmail.com>
2024-04-11 18:37:30 -0400

    update init.py

--- j8/init.py
+++ j8/init.py
@@ -6,7 +6,7 @@ import json
 
 comm.initialize()
 
-sim.createSimulateAnalyze(netParams=netParams,
+sim.createSimulate(netParams=netParams,
                           simConfig=cfg)
 
 results = sim.analysis.popAvgRates(show=False)
@@ -14,24 +14,11 @@ results = sim.analysis.popAvgRates(show=False)
 #inputs = specs.get_mappings()
 #out_json = json.dumps({**inputs, **rates})
 
-results['S_loss'] = (results['S'] - 10.2)**2
-results['M_loss'] = (results['M'] - 12.9)**2
+#results['S_loss'] = (results['S'] - 10.2)**2
+#results['M_loss'] = (results['M'] - 12.9)**2
 out_json = json.dumps(results)
 
+print(out_json)
 #TODO put all of this in a single function.
 comm.send(out_json)
 comm.close()
-
-
-
-
-
-from netpyne import sim
-from ca3 import netParams, cfg
-
-"""
-netParams.connParams['BC->BC_GABA'  ]['weight'] = 0
-netParams.connParams['BC->PYR_GABA' ]['weight'] = 0
-netParams.connParams['OLM->PYR_GABA']['weight'] = 0
-"""
-sim.createSimulate(netParams, cfg)
